0b740f4f855af90334408d74a9f8cfd33650d66c
fmiat <eee@>
2023-05-15 12:31:05 +0300

    решение задачи из блока 4

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		Server server = new Server(8080);
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+		context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+		server.setHandler(context);
+
+		server.start();
+		java.util.logging.Logger.getGlobal().info("Server started");
+		server.join();
+	}
+}
